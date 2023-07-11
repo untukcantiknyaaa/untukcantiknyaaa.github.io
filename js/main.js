79b434b7a6826df4271af8e3b96e9c8bfd21fbcd
benditandayusaputra <benditandayusaputra@gmail.com>
2023-07-12 00:58:13 +0700

    first commit project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$("#time")
+  .countdown("2023/07/12 00:43:00")
+  .on("update.countdown", function (event) {
+    var $this = $(this).html(
+      event.strftime(
+        "" +
+          '<span class="h1 font-weight-bold">%D</span> Hari' +
+          '<span class="h1 font-weight-bold">%H</span> Jam' +
+          '<span class="h1 font-weight-bold">%M</span> Menit' +
+          '<span class="h1 font-weight-bold">%S</span> Detik'
+      )
+    );
+  })
+  .on("finish.countdown", function (event) {
+    window.location.href = "otw.html";
+  });
